7b39b807091fe43d76dc35fdde22592f1c7c80a6
joerandy <33285293+joerandy@users.noreply.github.com>
2018-10-15 14:32:44 +0200

    Implemented database.addEntry

--- 3.Semestprojkt/3.Semestprojkt/database.cpp
+++ 3.Semestprojkt/3.Semestprojkt/database.cpp
@@ -10,3 +10,40 @@ database::database()
 database::~database()
 {
 }
+
+void database::addEntry(std::string dateTime, double ballXpos, double ballYpos, double cupXpos, double cupYpos, bool success)
+{
+	
+	try {
+		sql::Driver *driver;
+		sql::Connection *con;
+		sql::PreparedStatement *pstmt;
+	
+		/* Create a connection */
+		driver = get_driver_instance();
+		con = driver->connect("tcp://127.0.0.1:3306", "root", "kjq2ui7l");
+		/* Connect to the MySQL test database */
+		con->setSchema("test");
+
+		/* '?' is the supported placeholder syntax */
+		pstmt = con->prepareStatement("INSERT test_Data VALUES (CURRENT_TIMESTAMP,?,?,?,?,?)");
+		
+		//pstmt->setDateTime(1, dateTime); // USING PLACEHOLDER VALUE TO SHOW FORMAT, REPLACE WITH dateTime PARAMETER
+		pstmt->setDouble(1, ballXpos);
+		pstmt->setDouble(2, ballYpos);
+		pstmt->setDouble(3, cupXpos);
+		pstmt->setDouble(4, cupYpos);
+		pstmt->setBoolean(5, success);
+		pstmt->executeUpdate();
+		delete pstmt;
+		delete con;
+
+	}
+	catch (sql::SQLException &e) {
+		std::cout << "# ERR: SQLException in " << __FILE__;
+		std::cout << "(" << __FUNCTION__ << ") on line "	<< __LINE__ << std::endl;
+		std::cout << "# ERR: " << e.what();
+		std::cout << " (MySQL error code: " << e.getErrorCode();
+		std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+	}
+}
--- 3.Semestprojkt/3.Semestprojkt/database.h
+++ 3.Semestprojkt/3.Semestprojkt/database.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <stdlib.h>
+#include <iostream>
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+*/
+#include <jdbc/mysql_connection.h>
+#include <jdbc/mysql_driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+#include <jdbc/cppconn/prepared_statement.h>
+
 class database
 {
 public:
 	database();
 	~database();
+	void addEntry(std::string dateTime, double ballXpos, double ballYpos, double cupXpos, double cupYpos, bool success);
+	//void return type - maybe change to bool to return false if data is not saved
+private:
+
 };
 
