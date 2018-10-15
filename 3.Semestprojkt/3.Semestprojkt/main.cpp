e573b20d54f442d7f32d2bc66f0a052f7b92fe54
rafha13 <rafha13@student.sdu.dk>
2018-10-15 11:12:41 +0200

    Update 3.Semestprojkt/3.Semestprojkt/main.cpp

--- 3.Semestprojkt/3.Semestprojkt/main.cpp
+++ 3.Semestprojkt/3.Semestprojkt/main.cpp
@@ -15,7 +15,9 @@ using namespace cv;
 int main() {
 
 
-	VideoCapture cap(0); // open the default camera
+	VideoCapture cap(1); // open the default camera (0), (1) for USB
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1448);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
 	if (!cap.isOpened())  // check if we succeeded
 		return -1;
 
@@ -25,12 +27,18 @@ int main() {
 	{
 		Mat frame;
 		cap >> frame; // get a new frame from camera
-		cvtColor(frame, edges, CV_BGR2GRAY);
-		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-		Canny(edges, edges, 0, 30, 3);
-		imshow("edges", edges);
+
+
+
+		//cvtColor(frame, edges, CV_BGR2GRAY);
+	//	GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+	//	Canny(edges, edges, 0, 30, 3);
+
+		imshow("edges", frame);
 		if (waitKey(30) >= 0) break;
 	}
+
+	
 
 	getchar();
 	return 0;
